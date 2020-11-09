5c8d076ed156dd7eda03604975eb415b49af5b5e
ThierryMajutec <43922982+ThierryMajutec@users.noreply.github.com>
2020-11-09 21:31:46 +0100

    initial project

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-<script>
+import React from "react";
+import ReactDOM from "react-dom";
+
+//JSX
+function MyApp() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
+}
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
